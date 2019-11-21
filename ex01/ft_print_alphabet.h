032c08670cd8eaaeaca58daca8c6f825642166a1
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-21 15:44:03 -0300

    c_piscine_c_00 Add exercise 01
    - Resolve exercise 01 using just 'write' as asked
    - Use main file to test it
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex01/ft_print_alphabet.h
@@ -0,0 +1,4 @@
+#ifndef __STRUCTURES_H
+#define __STRUCTURES_H
+void ft_print_alphabet(void);
+#endif
---
+++ ex01/main.c
@@ -0,0 +1,9 @@
+#include "ft_print_alphabet.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc == 1)
+	{
+		ft_print_alphabet();
+	}
+}
