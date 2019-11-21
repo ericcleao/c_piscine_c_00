7d90bd1959265cff2a5a5ba124178cd0c01046c6
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-21 15:42:16 -0300

    c_piscine_c_00 Add exercise 00
    - Resolve exercise 00 using just 'write' as asked
    - Use main file to test it
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex00/ft_putchar.h
@@ -0,0 +1,4 @@
+#ifndef __STRUCTURES_H
+#define __STRUCTURES_H
+void ft_putchar(char c);
+#endif
---
+++ ex00/main.c
@@ -0,0 +1,9 @@
+#include "ft_putchar.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc == 2)
+	{
+		ft_putchar(argv[1][0]);
+	}
+}
