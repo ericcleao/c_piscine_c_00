f1076532ba49789a95b2270682d2aedfd2c6bc07
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-26 08:43:15 -0300

    Fix exercise 07
    - Consider the negative numbers when tried to print them
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 21:11:23 by ecerquei          #+#    #+#             */
-/*   Updated: 2019/11/21 21:11:25 by ecerquei         ###   ########.fr       */
+/*   Updated: 2019/11/26 08:42:07 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ void	print_char(char character);
 
 void	ft_putnbr(int nb)
 {
-	if (nb != 0)
+	if (nb > 0)
 	{
 		ft_putnbr(nb / 10);
 		print_char('0' + nb % 10);
+	}
+	else if (nb < 0)
+	{
+		print_char('-');
+		ft_putnbr((nb / 10) * -1);
+		print_char('0' + (nb % 10) * -1);
 	}
 }
 
