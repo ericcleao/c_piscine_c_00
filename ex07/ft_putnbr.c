bc474ef5143255b1c7b9bd7fc9c08303d44dd65d
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-26 14:19:25 -0300

    c_piscine_c_00 Fix exercise 07
    - Print the character '0' if the nbr is equals to '0'
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 21:11:23 by ecerquei          #+#    #+#             */
-/*   Updated: 2019/11/26 08:42:07 by ecerquei         ###   ########.fr       */
+/*   Updated: 2019/11/26 14:14:59 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,20 @@ void	print_char(char character);
 
 void	ft_putnbr(int nb)
 {
-	if (nb > 0)
+	char current_digit;
+
+	current_digit = '0' + nb % 10;
+	if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
-		print_char('0' + nb % 10);
 	}
 	else if (nb < 0)
 	{
 		print_char('-');
 		ft_putnbr((nb / 10) * -1);
-		print_char('0' + (nb % 10) * -1);
+		current_digit = '0' + ((nb % 10) * -1);
 	}
+	print_char(current_digit);
 }
 
 void	print_char(char character)
